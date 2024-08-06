2b80173c73f9e1f51ccc0aa7e96828d935a9a155
Jeremiah Longmatey <longmateyjeremiah1@gmail.com>
2024-08-06 09:14:26 +0000

    .

--- src/features/Hero.jsx
+++ src/features/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Flex, Heading } from '@chakra-ui/react'
+import { Button, Flex, Heading, Text } from '@chakra-ui/react'
 import { motion, useInView } from 'framer-motion'
 import StaggeredText from '../components/ui/StaggeredText'
 import { AppRoutes } from '../utils';
@@ -17,9 +17,10 @@ export const Hero = () => {
                 animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: 50 }}
                 transition={{ duration: 1.3, ease: 'easeInOut' }}
             >
+                <Text align='center'>Hello there</Text>
                 <Heading fontWeight='700' size={['lg', '2xl']} textAlign='center'>I'm Jeremiah Longmatey, <br />a Creative Frontend Engineer. </Heading>
             </motion.div>
-            <StaggeredText text="I am passionate about crafting user-friendly products that make human lives easier." />
+            <StaggeredText text="I'm passionate about crafting user-friendly products that make human lives easier." />
             <Button
                 variant='primary'
                 as="a"

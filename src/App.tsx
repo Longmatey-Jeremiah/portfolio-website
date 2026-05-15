ffae99065afe54aa9eab112d16339fa18f2fcb42
Longmatey Jeremiah <longmateyjeremiah1@gmail.com>
2026-05-15 18:13:53 +0000

    refactor: remove App.css and mount app through router shell
    Drop global App styles and render the themed router tree as the application root.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { Home } from "./pages";
-import { Navbar } from "./components";
-import { ThemeProvider } from "./providers";
+import { ThemeProvider } from "@/providers";
+import Router from "./routes";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="flex min-h-svh flex-col items-center justify-center w-full dark:text-white font-sans">
-        <Navbar />
-        <Home />
-      </div>
+    <ThemeProvider defaultTheme="dark" storageKey="jl-theme">
+      <Router />
     </ThemeProvider>
   );
 }
